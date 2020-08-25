7dacddbc2e1c58312665d628594aea414d68c7db
Martin Nørby Høj Jørgensen <mnj@eucsyd.dk>
2020-08-25 14:49:20 +0200

    Opgave 8.2

--- BethanysPieShopMobile/BethanysPieShopMobile/Project/RegisterView.xaml.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile/Project/RegisterView.xaml.cs
@@ -16,11 +16,39 @@ namespace BethanysPieShopMobile.Project
         {
             InitializeComponent();
             BirthDayDatePicker.Date = DateTime.Now.Date;
+            RegisterButton.IsEnabled = false;
         }
 
-        private void RegisterButton_Clicked(object sender, EventArgs e)
+        private async void RegisterButton_Clicked(object sender, EventArgs e)
         {
+            if (!String.IsNullOrEmpty(UserNameEntry.Text) && !String.IsNullOrEmpty(Password.Text))
+            {
+                await DisplayAlert("Alert", "You have registered successfully", "OK");
+            }
+        }
 
+        private async void BirthDayDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            if (BirthDayDatePicker.Date.Day == DateTime.Now.Day)
+            {
+                await DisplayAlert("Alert", "You weren't born today I guess", "OK");
+            }
+        }
+
+        private void UserNameEntry_Completed(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(UserNameEntry.Text) && !String.IsNullOrEmpty(Password.Text))
+            {
+                RegisterButton.IsEnabled = true;
+            }
+        }
+
+        private void Password_Completed(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(UserNameEntry.Text) && !String.IsNullOrEmpty(Password.Text))
+            {
+                RegisterButton.IsEnabled = true;
+            }
         }
     }
 }
