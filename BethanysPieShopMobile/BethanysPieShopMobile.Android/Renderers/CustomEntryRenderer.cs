698d64f8c01dc0b0cc5478a1b680ab5239f47694
Allf3 <martinnybolvej@gmail.com>
2020-08-26 14:00:36 +0200

    ne

---
+++ BethanysPieShopMobile/BethanysPieShopMobile.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BethanysPieShopMobile.Droid.Renderers;
+using BethanysPieShopMobile.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//CustomeEntry = Oppe i Shared Folder, Her definere vi den i det indeviuelle Nativ app
+//Opgave 7 CustomeRenderer
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace BethanysPieShopMobile.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control?.SetBackgroundColor(Android.Graphics.Color.WhiteSmoke);
+        }
+    }
+}
---
+++ BethanysPieShopMobile/BethanysPieShopMobile/Renderers/CustomEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BethanysPieShopMobile.Renderers
+{
+    public class CustomEntry: Entry
+    {
+        //Placeholder klasse, blir først rigtig brugt
+        //nede i de forskellige native projekter.
+    }
+}
